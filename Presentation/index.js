1ce9b96cb3877d33c7097818ec84e884a4ecf484
Samuel Márquez Ruiz <samuelmarquez588@gmail.com>
2025-04-24 10:21:28 +0200

    Update-Presentation

--- Presentation/index.js
+++ Presentation/index.js
@@ -3,6 +3,9 @@ const h1 = document.querySelector('#fullname');
 const button2 = document.getElementById('boton2');
 const body = document.querySelector('body');
 const button3 = document.getElementById('boton3');
+const button4 = document.getElementById('hobbiesboton');
+let hobbies = ['Video games', 'Basketball', 'Coding'];
+var hobbiesList = document.getElementById('hobbies');
 
 button.addEventListener('click', function(){
     var h1VAR = h1.textContent;
@@ -28,3 +31,22 @@ button3.addEventListener('click', function(){
         body.style.backgroundColor = '';
     }
 });
+button4.addEventListener('click', function(){
+    if (hobbiesList.children.length === 0) {
+        manageList(hobbiesList,hobbies); 
+        // for (let i = 0; i < hobbies.length; i++) {
+        //     const li = document.createElement('li');
+        //     li.innerText = hobbies[i];
+        //     hobbiesList.appendChild(li);
+        // }
+    } else {
+        hobbiesList.innerHTML = '';
+    }
+});
+function manageList(list,Element){
+    for (let i = 0; i < Element.length; i++) {
+        const li = document.createElement('li');
+        li.innerText = Element[i];
+        list.appendChild(li);
+    }
+}
